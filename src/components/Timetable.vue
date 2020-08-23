307df08ad5026ce30bfede578aad42b800ed74f9
Nicolas Gehring <n.gehring@online.de>
2020-08-23 15:59:27 +0200

    added timetable events

--- src/components/Timetable.vue
+++ src/components/Timetable.vue
@@ -1,67 +1,55 @@
 <template>
-  <div>
-    <h2>Timetable</h2>
-    <table id="tableId">
-      <thead>
-        <tr>
-          <th>Wann?</th>
-          <th>Montag</th>
-          <th>Dienstag</th>
-          <th>Mittwoch</th>
-          <th>Donnerstag</th>
-          <th>Freitag</th>
-          <th>Samstag</th>
-          <th>Sonntag</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="item in events" v-bind:key="item.type">
-          <td
-            v-for="act in item.activities"
-            v-bind:key="act"
-            v-bind:class="act.type"
-          >
-            {{ act }}
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+  <section class="bg-light" id="timetable">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 text-center">
+          <h2 class="section-heading text-uppercase">Timetable</h2>
+          <h3 class="section-subheading text-muted">
+            Bei Fragen bezüglich genauer Uhrzeiten bitte einen von uns
+            kontaktieren. <br />
+            <b
+              >Rot -> Teilnahme nichtmehr möglich Gelb -> Teilnehmerzahl begrenz
+              bei interesse melden</b
+            >
+          </h3>
+        </div>
+      </div>
+      <div class="row">
+        <table id="tableId">
+          <thead>
+            <tr>
+              <th>Wann?</th>
+              <th>Montag</th>
+              <th>Dienstag</th>
+              <th>Mittwoch</th>
+              <th>Donnerstag</th>
+              <th>Freitag</th>
+              <th>Samstag</th>
+              <th>Sonntag</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in events" v-bind:key="item.type">
+              <td
+                v-for="act in item.activities"
+                v-bind:key="act"
+                v-bind:class="act.type"
+              >
+                {{ act.name }}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+var jsonEventData = require("./events.json");
 export default {
   data() {
-    return {
-      events: [
-        {
-          type: "morgens",
-          activities: [
-            { name: "Morgens", type: "all" },
-            "---",
-            "Chill",
-            "GranFondo",
-            "Chill",
-            "Chill",
-            "Chill",
-            "Klettersteig",
-          ],
-        },
-        {
-          type: "mittags",
-          activities: [
-            "Mittags",
-            "---",
-            "Chill",
-            "GranFondo",
-            "Chill",
-            "Chill",
-            "Chill",
-            "Klettersteig",
-          ],
-        },
-      ],
-    };
+    return jsonEventData;
   },
 };
 </script>
@@ -77,7 +65,14 @@ tr,
 td {
   border: 1px solid #000;
 }
-.all {
-  background-color: aqua;
+
+tr {
+  height: 75px;
+}
+.restricted {
+  background-color: yellow;
+}
+.closed {
+  background-color: red;
 }
 </style>
